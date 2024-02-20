64a4dc766b58b2fafc88e3dc0fdf4845fb3cc602
NaturalZh <zhouziran@jikedingzhi.com>
2024-02-20 14:41:53 +0800

    modified tapping term setting and default debounce time

--- keyboards/skyloong/gk61/pro_48/config.h
+++ keyboards/skyloong/gk61/pro_48/config.h
@@ -8,9 +8,8 @@
 #define WIN_MOD_INDEX 16
 #define MAC_MOD_INDEX 17
 #define SDB A4
+#define TAPPING_TERM_PER_KEY
 
-#define RGB_DISABLE_WHEN_USB_SUSPENDED
 #define RGB_MATRIX_KEYPRESSES
 #define RGB_MATRIX_FRAMEBUFFER_EFFECTS
 
-#define g_suspend_state rgb_matrix_get_suspend_state()
