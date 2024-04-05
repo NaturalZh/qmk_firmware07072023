e219df79e92d5eed5189d626e77650aacdaaf42b
NaturalZh <zhouziran@jikedingzhi.com>
2024-04-05 15:53:55 +0800

    Create GK104 Backlight ANSI layout keyboard.

---
+++ keyboards/skyloong/gk104/bl/config.h
@@ -0,0 +1,10 @@
+// Copyright 2023 JZ-Skyloong (@JZ-Skyloong)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#ifdef ENCODER_ENABLE
+#    define ENCODER_MAP_KEY_DELAY 10
+#endif
+
+
