f14000f020d9d0ee0058a8e904b4bbb3016174ad
NaturalZh <zhouziran@jikedingzhi.com>
2024-11-30 15:27:03 +0800

    update skyloong/gk75/bl/ansi/keymaps/default/keymap.c

--- keyboards/skyloong/gk75/bl/ansi/keymaps/default/keymap.c
+++ keyboards/skyloong/gk75/bl/ansi/keymaps/default/keymap.c
@@ -11,7 +11,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,        KC_Y,     KC_U,       KC_I,     KC_O,        KC_P,     KC_LBRC,  KC_RBRC,   KC_BSLS,   KC_PGUP,
         KC_CAPS,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,        KC_H,     KC_J,       KC_K,     KC_L,     KC_SCLN,     KC_QUOT,              KC_ENT,   KC_PGDN,
         KC_LSFT,     KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,        KC_N,     KC_M,    KC_COMM,   KC_DOT,     KC_SLSH,     KC_RSFT,   KC_F11,     KC_UP,
-        KC_LCTL,  KC_LGUI,  KC_LALT,             KC_SPC,   KC_SPC,      KC_SPC,   KC_SPC,              KC_RALT,       MO(1),     KC_RCTL,  KC_LEFT,   KC_DOWN,   KC_RGHT
+        KC_LCTL,  KC_LGUI,  KC_LALT,                       KC_SPC,      KC_SPC,   KC_SPC,              KC_RALT,       MO(1),     KC_RCTL,  KC_LEFT,   KC_DOWN,   KC_RGHT
     ),
 
     [1] = LAYOUT_all(
@@ -20,13 +20,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  _______,  _______,  _______,  _______,  _______,     _______,   _______,   _______,  _______,    _______,      _______,  _______,  _______,    KC_HOME,
         _______,  _______,  _______,  _______,  _______,  _______,     _______,   _______,   _______,  _______,    _______,      _______,            _______,     KC_END,
         _______,  _______,  _______,  _______,  _______,  _______,     _______,   _______,   _______,  _______,    _______,      _______,              BL_UP,
-        _______,  _______,  _______,            _______,  _______,     _______,   _______,             _______,    _______,      _______,   BL_OFF,  BL_DOWN,     BL_ON
+        _______,  _______,  _______,                      _______,     _______,   _______,             _______,    _______,      _______,   BL_OFF,  BL_DOWN,     BL_ON
     ),
 };
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(BL_DOWN, BL_UP), ENCODER_CCW_CW(C(KC_WH_D), C(KC_WH_U)), ENCODER_CCW_CW(G(C(KC_LEFT)),G(C(KC_RGHT))) },
-    [1] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(BL_DOWN, BL_UP), ENCODER_CCW_CW(C(KC_WH_D), C(KC_WH_U)), ENCODER_CCW_CW(G(C(KC_LEFT)),G(C(KC_RGHT))) }
+    [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(BL_DOWN, BL_UP), ENCODER_CCW_CW(C(MS_WHLD), C(MS_WHLU)), ENCODER_CCW_CW(G(C(KC_LEFT)),G(C(KC_RGHT))) },
+    [1] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(BL_DOWN, BL_UP), ENCODER_CCW_CW(C(MS_WHLD), C(MS_WHLU)), ENCODER_CCW_CW(G(C(KC_LEFT)),G(C(KC_RGHT))) }
 };
 #endif
